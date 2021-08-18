49df14474ad5b3ab07cea33cd91a83b57d264b67
bogutskii <sl9site@gmail.com>
2021-08-18 12:38:24 -0700

    add post page and DB

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///blog.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Article %r>' % self.id
 
 
 @app.route('/')
@@ -8,6 +25,36 @@ def hello():
     return render_template('index.html')
 
 
+@app.route('/create_post', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'INPUT ERROR!'
+    else:
+        return render_template('create_post.html')
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date).all()
+
+    return render_template('posts.html', articles=articles)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
