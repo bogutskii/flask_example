0ad186ee41fbe9f9fef72fc1bc37421b84796cd1
bogutskii <sl9site@gmail.com>
2021-09-02 17:24:15 -0700

    auth and dashboard

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
@@ -15,6 +15,15 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "denisdziganchuk"
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,9 +31,6 @@ class Article(db.Model):
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return 'Article %r>' % self.id
 
 
 class User(db.Model, UserMixin):
@@ -122,7 +128,19 @@ def update_post(id):
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template("login.html", form=form)
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -133,6 +151,7 @@ def register():
         new_user = User(username=form.username.data, password=hash_password)
         db.session.add(new_user)
         db.session.commit()
+        print('pas:', hash_password)
         return redirect(url_for("login"))
     return render_template("registration.html", form=form)
 
